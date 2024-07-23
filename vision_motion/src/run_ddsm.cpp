15307cf2bd714b7626e4ff101877cca2c3e6e104
Kim <jeremy3771@naver.com>
2024-07-23 21:51:40 +0900

    Update run_ddsm.cpp

--- vision_motion/src/run_ddsm.cpp
+++ vision_motion/src/run_ddsm.cpp
@@ -15,10 +15,14 @@ using std::placeholders::_1;
 class DDSM_Twist : public rclcpp::Node {
 public:
     DDSM_Twist() : Node("DDSM115"), port1(0), port2(2), port3(4), port4(6){
-        declare_parameter("WB", 0.47);
-        declare_parameter("AW", 0.38);
+        declare_parameter("WO1", 0.36);
+        declare_parameter("WO2", 0.055);
+        declare_parameter("WO3", 0.185);
+        declare_parameter("AW", 0.40);
         declare_parameter("WD", 0.1);
-        get_parameter("WB", wheelbase_);
+        get_parameter("WO1", wheelOffset1_);
+        get_parameter("WO2", wheelOffset2_);
+        get_parameter("WO3", wheelOffset3_);
         get_parameter("AW", axleWidth_);
         get_parameter("WD", wheelDiameter_);
 
@@ -29,8 +33,16 @@ private:
     void timer_cb() {
         port1.SET_VELOCITY(1, motor_rpm_[0]);
         port2.SET_VELOCITY(1, motor_rpm_[1]);
-        port3.SET_VELOCITY(1, motor_rpm_[2]);
-        port4.SET_VELOCITY(1, motor_rpm_[3]);
+        if (timer == 0) {
+            port3.SET_VELOCITY(1, motor_rpm_[2]);
+            port4.SET_VELOCITY(1, motor_rpm_[4]);
+            timer = 1;
+        }
+        else {
+            port3.SET_VELOCITY(2, motor_rpm_[3]);
+            port4.SET_VELOCITY(2, motor_rpm_[5]);
+            timer = 0;
+        }
     }
 
     void twist_cb(const geometry_msgs::msg::Twist msg) {
@@ -39,8 +51,13 @@ private:
         auto radius = std::abs(v / omega);
         double cf = PI * wheelDiameter_;
 
-        double omega1 = v * sqrt(pow((radius - axleWidth_ / 2), 2) + pow((wheelbase_ / 2), 2)) / radius / wheelDiameter_ * 2;
-        double omega2 = v * sqrt(pow((radius + axleWidth_ / 2), 2) + pow((wheelbase_ / 2), 2)) / radius / wheelDiameter_ * 2;
+        wheel_omega[0] = v * sqrt(pow((radius - axleWidth_ / 2), 2) + pow((wheelOffset1_ / 2), 2)) / radius / wheelDiameter_ * 2;
+        wheel_omega[1] = v * sqrt(pow((radius + axleWidth_ / 2), 2) + pow((wheelOffset1_ / 2), 2)) / radius / wheelDiameter_ * 2;
+        wheel_omega[2] = v * sqrt(pow((radius - axleWidth_ / 2), 2) + pow((wheelOffset2_ / 2), 2)) / radius / wheelDiameter_ * 2;
+        wheel_omega[3] = v * sqrt(pow((radius + axleWidth_ / 2), 2) + pow((wheelOffset2_ / 2), 2)) / radius / wheelDiameter_ * 2;
+        wheel_omega[4] = v * sqrt(pow((radius - axleWidth_ / 2), 2) + pow((wheelOffset3_ / 2), 2)) / radius / wheelDiameter_ * 2;
+        wheel_omega[5] = v * sqrt(pow((radius + axleWidth_ / 2), 2) + pow((wheelOffset3_ / 2), 2)) / radius / wheelDiameter_ * 2;
+
 
         if (omega < -0.01) {
             motor_rpm_[0] = omega2 * 30 / PI;
@@ -55,18 +72,22 @@ private:
             motor_rpm_[3] = -1 * omega2 * 30 / PI;
         }
         else {
-            motor_rpm_[0] = v / cf * 30 / PI;
-            motor_rpm_[1] = -1 * v / cf * 30 / PI;
-            motor_rpm_[2] = v / cf * 30 / PI;
-            motor_rpm_[3] = -1 * v / cf * 30 / PI;
+            motor_rpm_[0] = (60 * v) / (wheelDiameter_ * PI);
+            motor_rpm_[1] = -1 * (60 * v) / (wheelDiameter_ * PI);
+            motor_rpm_[2] = (60 * v) / (wheelDiameter_ * PI);
+            motor_rpm_[3] = -1 * (60 * v) / (wheelDiameter_ * PI);
+            motor_rpm_[4] = (60 * v) / (wheelDiameter_ * PI);
+            motor_rpm_[5] = -1 * (60 * v) / (wheelDiameter_ * PI);
         }
     }
 
     rclcpp::TimerBase::SharedPtr timer_;
     rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr twist_sub_;
-    double motor_rpm_[4] = {0.0, }; // LF, RF, LR, RR
+    double wheel_omega[6];
+    double motor_rpm_[6] = {0.0, }; // LF, RF, LM, RM, LR, RR
     MOTOR_COMMAND port1, port2, port3, port4;
-    double wheelbase_, axleWidth_, wheelDiameter_;
+    double wheelOffset1_, wheelOffset2_, wheelOffset3_,axleWidth_, wheelDiameter_;
+    int timer = 0; // 0: middle wheel, 1: rear wheel
 };
 
 int main(int argc, char * argv[]) {
