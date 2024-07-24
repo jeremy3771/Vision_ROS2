274475a8758737f3163149f2459e9c882740e2ee
Kim <jeremy3771@naver.com>
2024-07-24 14:46:16 +0900

    Update motor_driver.cpp

--- vision_motion/src/motor_driver.cpp
+++ vision_motion/src/motor_driver.cpp
@@ -71,54 +71,53 @@ bool MotorCommand::setTorque(bool enable) {
 bool MotorCommand::writePosition(const int64_t value[6]) {
     bool dxl_addparam_result;
     int8_t dxl_comm_result;
-    uint8_t data_byte[6][4] = {0, };
+    uint8_t data_byte[4] = {0, };
 
-    data_byte[0][0] = DXL_LOBYTE(DXL_LOWORD(value[0]));
-    data_byte[0][1] = DXL_HIBYTE(DXL_LOWORD(value[0]));
-    data_byte[0][2] = DXL_LOBYTE(DXL_HIWORD(value[0]));
-    data_byte[0][3] = DXL_HIBYTE(DXL_HIWORD(value[0]));
-
-    data_byte[1][0] = DXL_LOBYTE(DXL_LOWORD(value[1]));
-    data_byte[1][1] = DXL_HIBYTE(DXL_LOWORD(value[1]));
-    data_byte[1][2] = DXL_LOBYTE(DXL_HIWORD(value[1]));
-    data_byte[1][3] = DXL_HIBYTE(DXL_HIWORD(value[1]));
-
-    data_byte[2][0] = DXL_LOBYTE(DXL_LOWORD(value[2]));
-    data_byte[2][1] = DXL_HIBYTE(DXL_LOWORD(value[2]));
-    data_byte[2][2] = DXL_LOBYTE(DXL_HIWORD(value[2]));
-    data_byte[2][3] = DXL_HIBYTE(DXL_HIWORD(value[2]));
-
-    data_byte[3][0] = DXL_LOBYTE(DXL_LOWORD(value[3]));
-    data_byte[3][1] = DXL_HIBYTE(DXL_LOWORD(value[3]));
-    data_byte[3][2] = DXL_LOBYTE(DXL_HIWORD(value[3]));
-    data_byte[3][3] = DXL_HIBYTE(DXL_HIWORD(value[3]));
-
-    data_byte[4][0] = DXL_LOBYTE(DXL_LOWORD(value[4]));
-    data_byte[4][1] = DXL_HIBYTE(DXL_LOWORD(value[4]));
-    data_byte[4][2] = DXL_LOBYTE(DXL_HIWORD(value[4]));
-    data_byte[4][3] = DXL_HIBYTE(DXL_HIWORD(value[4]));
-
-    data_byte[5][0] = DXL_LOBYTE(DXL_LOWORD(value[5]));
-    data_byte[5][1] = DXL_HIBYTE(DXL_LOWORD(value[5]));
-    data_byte[5][2] = DXL_LOBYTE(DXL_HIWORD(value[5]));
-    data_byte[5][3] = DXL_HIBYTE(DXL_HIWORD(value[5]));
-
-    dxl_addparam_result = groupSyncWritePosition_->addParam(DXL_F_LEFT_ID, (uint8_t*)&data_byte[0]);
+    data_byte[0] = DXL_LOBYTE(DXL_LOWORD(value[0]));
+    data_byte[1] = DXL_HIBYTE(DXL_LOWORD(value[0]));
+    data_byte[2] = DXL_LOBYTE(DXL_HIWORD(value[0]));
+    data_byte[3] = DXL_HIBYTE(DXL_HIWORD(value[0]));
+    dxl_addparam_result = groupSyncWritePosition_->addParam(DXL_F_LEFT_ID, (uint8_t*)&data_byte);
     if (dxl_addparam_result != true)
         return false;
-    dxl_addparam_result = groupSyncWritePosition_->addParam(DXL_F_RIGHT_ID, (uint8_t*)&data_byte[1]);
+
+    data_byte[0] = DXL_LOBYTE(DXL_LOWORD(value[1]));
+    data_byte[1] = DXL_HIBYTE(DXL_LOWORD(value[1]));
+    data_byte[2] = DXL_LOBYTE(DXL_HIWORD(value[1]));
+    data_byte[3] = DXL_HIBYTE(DXL_HIWORD(value[1]));
+    dxl_addparam_result = groupSyncWritePosition_->addParam(DXL_F_RIGHT_ID, (uint8_t*)&data_byte);
     if (dxl_addparam_result != true)
         return false;
-    dxl_addparam_result = groupSyncWritePosition_->addParam(DXL_M_LEFT_ID, (uint8_t*)&data_byte[2]);
+
+    data_byte[0] = DXL_LOBYTE(DXL_LOWORD(value[2]));
+    data_byte[1] = DXL_HIBYTE(DXL_LOWORD(value[2]));
+    data_byte[2] = DXL_LOBYTE(DXL_HIWORD(value[2]));
+    data_byte[3] = DXL_HIBYTE(DXL_HIWORD(value[2]));
+    dxl_addparam_result = groupSyncWritePosition_->addParam(DXL_M_LEFT_ID, (uint8_t*)&data_byte);
     if (dxl_addparam_result != true)
         return false;
-    dxl_addparam_result = groupSyncWritePosition_->addParam(DXL_M_RIGHT_ID, (uint8_t*)&data_byte[3]);
+
+    data_byte[0] = DXL_LOBYTE(DXL_LOWORD(value[3]));
+    data_byte[1] = DXL_HIBYTE(DXL_LOWORD(value[3]));
+    data_byte[2] = DXL_LOBYTE(DXL_HIWORD(value[3]));
+    data_byte[3] = DXL_HIBYTE(DXL_HIWORD(value[3]));
+    dxl_addparam_result = groupSyncWritePosition_->addParam(DXL_M_RIGHT_ID, (uint8_t*)&data_byte);
     if (dxl_addparam_result != true)
         return false;
-    dxl_addparam_result = groupSyncWritePosition_->addParam(DXL_R_LEFT_ID, (uint8_t*)&data_byte[4]);
+
+    data_byte[0] = DXL_LOBYTE(DXL_LOWORD(value[4]));
+    data_byte[1] = DXL_HIBYTE(DXL_LOWORD(value[4]));
+    data_byte[2] = DXL_LOBYTE(DXL_HIWORD(value[4]));
+    data_byte[3] = DXL_HIBYTE(DXL_HIWORD(value[4]));
+    dxl_addparam_result = groupSyncWritePosition_->addParam(DXL_R_LEFT_ID, (uint8_t*)&data_byte);
     if (dxl_addparam_result != true)
         return false;
-    dxl_addparam_result = groupSyncWritePosition_->addParam(DXL_R_RIGHT_ID, (uint8_t*)&data_byte[5]);
+
+    data_byte[0] = DXL_LOBYTE(DXL_LOWORD(value[5]));
+    data_byte[1] = DXL_HIBYTE(DXL_LOWORD(value[5]));
+    data_byte[2] = DXL_LOBYTE(DXL_HIWORD(value[5]));
+    data_byte[3] = DXL_HIBYTE(DXL_HIWORD(value[5]));
+    dxl_addparam_result = groupSyncWritePosition_->addParam(DXL_R_RIGHT_ID, (uint8_t*)&data_byte);
     if (dxl_addparam_result != true)
         return false;
 
