1b7ddf30345566daaa4aba0a60b61e731e5b6ab5
Kim <jeremy3771@naver.com>
2024-07-24 15:05:49 +0900

    Update run_ddsm.cpp

--- vision_motion/src/run_ddsm.cpp
+++ vision_motion/src/run_ddsm.cpp
@@ -35,11 +35,11 @@ private:
         port2.SET_VELOCITY(1, motor_rpm_[1]);
         if (timer == 0) {
             port3.SET_VELOCITY(1, motor_rpm_[2]);
-            port4.SET_VELOCITY(1, motor_rpm_[4]);
+            port4.SET_VELOCITY(1, motor_rpm_[3]);
             timer = 1;
         }
         else {
-            port3.SET_VELOCITY(2, motor_rpm_[3]);
+            port3.SET_VELOCITY(2, motor_rpm_[4]);
             port4.SET_VELOCITY(2, motor_rpm_[5]);
             timer = 0;
         }
