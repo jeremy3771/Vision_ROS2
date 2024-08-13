818d8105094fff6d1b90ad09f03ac7c22dd9b667
Kim <jeremy3771@naver.com>
2024-08-13 17:23:02 +0900

    Update run_ddsm.cpp

--- vision_motion/src/run_ddsm.cpp
+++ vision_motion/src/run_ddsm.cpp
@@ -57,25 +57,23 @@ private:
         wheel_omega[4] = v * sqrt(pow((radius - axleWidth_ / 2), 2) + pow((wheelOffset3_ / 2), 2)) / radius / wheelDiameter_ * 2;
         wheel_omega[5] = v * sqrt(pow((radius + axleWidth_ / 2), 2) + pow((wheelOffset3_ / 2), 2)) / radius / wheelDiameter_ * 2;
 
-        if (std::abs(omega) > 0.01 && radius < 0.01) {
-	    if (omega > 0.01) {
-	    	motor_rpm_[0] = 2 * omega * 30 / PI;
-            motor_rpm_[1] = 2 * omega * 30 / PI;
-            motor_rpm_[2] = 0;
-            motor_rpm_[3] = 0;
-            motor_rpm_[4] = 2 * omega * 30 / PI;
-            motor_rpm_[5] = 2 * omega * 30 / PI;
-	    	}
-	    else if (omega < -0.01) {
-	    	motor_rpm_[0] =  2 * omega * 30 / PI;
-            motor_rpm_[1] =  2 * omega * 30 / PI;
-            motor_rpm_[2] =  0;
-            motor_rpm_[3] =  0;
-            motor_rpm_[4] =  2 * omega * 30 / PI;
-            motor_rpm_[5] =  2 * omega * 30 / PI;
-	    	    }
-            }      
-        else if (omega < -0.01 && radius > 0.01) {
+        if (std::abs(v) < 0.01 && omega < -0.01) { // CW in place
+            motor_rpm_[0] = -1 * sqrt(pow(wheelOffset1_ - wheelOffset2_ , 2) + pow(axleWidth_ / 2, 2)) * omega / wheelDiameter_ * 30 / PI;
+            motor_rpm_[1] = -1 * sqrt(pow(wheelOffset1_ - wheelOffset2_ , 2) + pow(axleWidth_ / 2, 2)) * omega / wheelDiameter_ * 30 / PI;
+            motor_rpm_[2] = -1 * (axleWidth_ / 2) * omega / wheelDiameter_ * 30 / PI;
+            motor_rpm_[3] = -1 * (axleWidth_ / 2) * omega / wheelDiameter_ * 30 / PI;
+            motor_rpm_[4] = -1 * sqrt(pow(wheelOffset1_ + wheelOffset2_ , 2) + pow(axleWidth_ / 2, 2)) * omega / wheelDiameter_ * 30 / PI;
+            motor_rpm_[5] = -1 * sqrt(pow(wheelOffset1_ + wheelOffset2_ , 2) + pow(axleWidth_ / 2, 2)) * omega / wheelDiameter_ * 30 / PI;
+        }
+        else if (std::abs(v) < 0.01 && omega > 0.01) { // CCW in place
+            motor_rpm_[0] = -1 * sqrt(pow(wheelOffset1_ - wheelOffset2_ , 2) + pow(axleWidth_ / 2, 2)) * omega / wheelDiameter_ * 30 / PI;
+            motor_rpm_[1] = -1 * sqrt(pow(wheelOffset1_ - wheelOffset2_ , 2) + pow(axleWidth_ / 2, 2)) * omega / wheelDiameter_ * 30 / PI;
+            motor_rpm_[2] = -1 * (axleWidth_ / 2) * omega / wheelDiameter_ * 30 / PI;
+            motor_rpm_[3] = -1 * (axleWidth_ / 2) * omega / wheelDiameter_ * 30 / PI;
+            motor_rpm_[4] = -1 * sqrt(pow(wheelOffset1_ + wheelOffset2_ , 2) + pow(axleWidth_ / 2, 2)) * omega / wheelDiameter_ * 30 / PI;
+            motor_rpm_[5] = -1 * sqrt(pow(wheelOffset1_ + wheelOffset2_ , 2) + pow(axleWidth_ / 2, 2)) * omega / wheelDiameter_ * 30 / PI;
+        }
+        else if (omega < -0.01) {
             motor_rpm_[0] = wheel_omega[1] * 30 / PI;
             motor_rpm_[1] = -1 * wheel_omega[0] * 30 / PI;
             motor_rpm_[2] = wheel_omega[3] * 30 / PI;
@@ -83,7 +81,7 @@ private:
             motor_rpm_[4] = wheel_omega[5] * 30 / PI;
             motor_rpm_[5] = -1 * wheel_omega[4] * 30 / PI;
         }
-        else if(omega > 0.01 && radius > 0.01) {
+        else if(omega > 0.01) {
             motor_rpm_[0] = wheel_omega[0] * 30 / PI;
             motor_rpm_[1] = -1 * wheel_omega[1] * 30 / PI;
             motor_rpm_[2] = wheel_omega[2] * 30 / PI;
